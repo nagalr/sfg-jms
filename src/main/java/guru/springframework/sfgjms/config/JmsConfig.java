d20023b6150b9d0551dad1fecb2d9db677f50c44
Ronnen Nagal <nagalr@gmail.com>
2021-07-26 19:37:37 +0300

    Add 'MappingJackson2MessageConverter' @Bean in JmsConfig.java

---
+++ src/main/java/guru/springframework/sfgjms/config/JmsConfig.java
@@ -0,0 +1,24 @@
+package guru.springframework.sfgjms.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+/**
+ * Created by ronnen on 26-Jul-2021
+ */
+
+@Configuration
+public class JmsConfig {
+
+    @Bean
+    public MessageConverter messageConverter() {
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName("_type");
+
+        return converter;
+    }
+}
